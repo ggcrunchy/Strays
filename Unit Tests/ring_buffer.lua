45b55837e9113e98329572c97d1e085fecbc19dd
Steven Johnson <steve@xibalbastudios.com>
2014-03-09 18:40:19 -0600

    Unit tests for ring buffer, from snippets

---
+++ Unit Tests/ring_buffer.lua
@@ -0,0 +1,55 @@
+--- Ring buffer tests.
+
+--
+-- Permission is hereby granted, free of charge, to any person obtaining
+-- a copy of this software and associated documentation files (the
+-- "Software"), to deal in the Software without restriction, including
+-- without limitation the rights to use, copy, modify, merge, publish,
+-- distribute, sublicense, and/or sell copies of the Software, and to
+-- permit persons to whom the Software is furnished to do so, subject to
+-- the following conditions:
+--
+-- The above copyright notice and this permission notice shall be
+-- included in all copies or substantial portions of the Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+-- EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+-- MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+-- IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+-- CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+-- TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+-- SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+--
+-- [ MIT license: http://www.opensource.org/licenses/mit-license.php ]
+--
+
+local rb = require("array_ops.ring_buffer")
+local aa,h,t,c,v = {0,0,0,0,0,0,0}
+print("E?", h==t)
+c,h,t=rb.Push(aa, 7, h, t)
+v,h,t=rb.Pop(aa, h, t)
+print("POP 7!", v, h, t)
+print("E?", h==t)
+c,h,t=rb.Push(aa, 6, h, t)
+print("E?", h==t)
+vdump(aa)
+c,h,t=rb.Push(aa, 70, h, t)
+vdump(aa)
+for i = 1, 8 do
+	c,h,t=rb.Push(aa, 20 + i, h, t)
+	if not c then
+		print("Full!", 20 + i)
+	end
+end
+vdump(aa)
+for i = 1, 3 do
+	v, h, t = rb.Pop(aa, h, t)
+end
+vdump(aa)
+for i = 1, 4 do
+	c, h, t = rb.Push(aa, 30 * i, h, t)
+	if not c then
+		print("Full!", 30 * i)
+	end
+end
+vdump(aa)
