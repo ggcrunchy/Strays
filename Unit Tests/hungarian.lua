18a9ab97ce04829795d2067506211fa9f029f48e
Steven Johnson <steve@xibalbastudios.com>
2014-05-04 22:59:57 -0500

    Code for recording reads and writes for dense Hungarian algorithm core, to cross-compare against expected diagonal results

--- Unit Tests/hungarian.lua
+++ Unit Tests/hungarian.lua
@@ -366,3 +366,215 @@ function DUMP (what, phase, qs)
 	print("")
 end
 --++++++++++++++++++++++++++++++++++++++++
+
+-- in hungarian, AuxRun():
+
+--[[
+local a={}
+
+local nreads, nwrites = 0, 0
+Costs = setmetatable({}, {
+	__index = function(t, k)
+			nreads=nreads+1
+		if not BB then
+		a[#a+1]=("read %i {%i}, (%s)"):format(k, DIAG[k] or -1, tostring(costs[k]))
+		end
+		return COSTS[k]
+	end,
+	__newindex = function(t, k, v)
+			nwrites=nwrites+1
+		if not BB then
+		a[#a+1]=("write %i {%i} (%s)"):format(k, DIAG[k] or -1, tostring(v))
+		end
+		COSTS[k] = v
+	end
+})
+DIAG={}
+DIAG[1], DIAG[2]=1, 2
+function DOING (what)
+	a[#a+1]=""
+	a[#a+1]=what
+end
+local index = ncols + 1
+local j = 3
+for _ = 2, n / ncols - 1 do
+	DIAG[index], DIAG[index+1],DIAG[index+2]=j,j+1,j+2
+	j=j+3
+	index = index + ncols + 1
+end
+DIAG[index],DIAG[index+1]=j,j+1
+
+--	return 
+	local u=AuxRun(dense, costs, n, ncols, n / ncols, opts)
+NN=NN+1
+if NN == 2 then
+	local fp = io.open(system.pathForFile("Out.txt", system.DocumentsDirectory), "w")
+	if fp then
+		fp:write("#READS = ", nreads, "\n")
+		fp:write("#WRITES = ", nwrites, "\n")
+		fp:write("\n")
+		for i = 1, #a do
+			fp:write(a[i], "\n")
+		end
+		fp:close()
+	end
+end
+	return u
+]]
+
+-- in dense:
+
+--[[
+--- DOCME
+function M.CorrectMin (costs, vmin, rows, col, rfrom, rto, nrows, ncols)
+	local ci, rindex = col + 1, 1
+DOING("correct min")
+	for row = rfrom, rto do
+		if rindex <= nrows and rows[rindex] == row then
+			rindex = rindex + 1
+		else
+if DIAG[ci] then
+			local cost = costs[ci]
+
+			if cost < vmin then
+				vmin = cost
+			end
+end
+		end
+
+		ci = ci + ncols
+	end
+
+	return vmin
+end
+
+--- DOCMEMORE
+-- Do enough columns contain a starred zero?
+function M.CountCoverage (row_star, n, ncols)
+	for ri = 1, n, ncols do
+		local col = row_star[ri]
+
+		if col < ncols and Clear(FreeColBits, col) then
+			CovColN, UncovColN = nil
+		end
+	end
+
+	return vector.AllClear(FreeColBits)
+end
+
+--- DOCMEMORE
+-- Attempts to find a zero among uncovered elements' costs
+function M.FindZero (costs, urows, ucn, urn, ncols, from, vmin)
+	local ucols = UncovCols
+DOING("find zero")
+	for i = from, urn do
+		local ri, vmin_cur = urows[i] * ncols + 1, vmin
+
+		for j = 1, ucn do
+			local col = ucols[j]
+if DIAG[ri+col] then
+			local cost = costs[ri + col]
+
+			if cost < vmin then
+				if cost == 0 then
+					return vmin_cur, ri, col, i
+				else
+					vmin = cost
+				end
+			end
+end
+		end
+	end
+
+	return vmin
+end
+
+--- DOCME
+function M.FindZeroInRow (costs, col_star, ri, ncols, np1)
+DOING("find zero in row")
+	for i = 0, ncols - 1 do
+if DIAG[ri+i] then
+		if costs[ri + i] == 0  and col_star[i + 1] == np1 then
+			return i
+		end
+end
+	end
+end
+
+--- DOCME
+function M.GetUncoveredColumns ()
+	return UncovColN or GetIndices_Set(UncovCols, FreeColBits)
+end
+
+--- DOCMEMORE
+-- Finds the smallest element in each row and subtracts it from every row element
+function M.SubtractSmallestRowCosts (costs, from, n, ncols)
+	local dcols = ncols - 1
+DOING("sub smallest row costs")
+	for ri = 1, n, ncols do
+		local rmin = math.huge--from[ri]
+
+		for i = 0, dcols do--1, dcols do
+if DIAG[ri+i] then
+			rmin = min(rmin, from[ri + i])
+end
+		end
+
+		for i = ri, ri + dcols do
+if DIAG[i] then
+			costs[i] = from[i] - rmin
+end
+		end
+	end
+end
+
+--- DOCME
+function M.UncoverColumn (col, ucn)
+	Set_Fast(FreeColBits, col)
+
+	-- Invalidate columns, since one became dirty. At the expense of some locality, a second
+	-- accumulation can be avoided (during priming) by appending the now-uncovered column to
+	-- the uncovered columns list.
+	UncovCols[ucn + 1] = col
+
+	CovColN, UncovColN = nil
+end
+
+--- DOCMEMORE
+-- Updates the cost of each element belonging to the cols x rows set
+function M.UpdateCovered (costs, vmin, rows, rn, ncols)
+	CovColN = CovColN or GetIndices_Clear(CovCols, FreeColBits)
+DOING("update covered")
+	local cols, cn = CovCols, CovColN
+
+	for i = 1, rn do
+		local ri = rows[i] * ncols + 1
+
+		for j = 1, cn do
+			local index = ri + cols[j]
+if DIAG[index] then
+			costs[index] = costs[index] + vmin
+end
+		end
+	end
+end
+
+--- DOCMEMORE
+-- Updates the cost of each element belonging to the cols x rows set
+function M.UpdateUncovered (costs, vmin, rows, rn, ncols)
+	UncovColN = UncovColN or GetIndices_Set(UncovCols, FreeColBits)
+DOING("update uncovered")
+	local cols, cn = UncovCols, UncovColN
+
+	for i = 1, rn do
+		local ri = rows[i] * ncols + 1
+
+		for j = 1, cn do
+			local index = ri + cols[j]
+if DIAG[index] then
+			costs[index] = costs[index] - vmin
+end
+		end
+	end
+end
+]]
